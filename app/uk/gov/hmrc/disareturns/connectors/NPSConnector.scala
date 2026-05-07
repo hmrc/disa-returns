ba6f0d0703063fce8aeebb5dff217fce98a84beb
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-05-07 14:43:33 +0100

    Updated download link to github direct link whilst not deployed to prod

--- app/uk/gov/hmrc/disareturns/connectors/NPSConnector.scala
+++ app/uk/gov/hmrc/disareturns/connectors/NPSConnector.scala
@@ -61,7 +61,7 @@ class NPSConnector @Inject() (httpClient: HttpClientV2, appConfig: AppConfig)(im
   def retrieveReconciliationReportPage(zReference: String, taxYear: String, month: Month, pageIndex: Int, pageSize: Int)(implicit
     hc:                                            HeaderCarrier
   ): EitherT[Future, UpstreamErrorResponse, HttpResponse] = {
-    val url = s"${appConfig.npsBaseUrl}/monthly/$zReference/$taxYear/${month.toString}/results?pageIndex=$pageIndex&pageSize=$pageSize"
+    val url = s"${appConfig.npsBaseUrl}/monthly/$zReference/$taxYear/${month.toString}/results?skip=$pageIndex&take=$pageSize"
     read(
       httpClient
         .get(url"$url")
